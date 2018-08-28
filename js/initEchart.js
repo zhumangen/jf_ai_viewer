874d5e5cd219f92ca9aa1d9ff09f35946c220129
Mangen.Zhu <zhumangen@163.com>
2018-08-28 15:45:38 +0800

    echats and styles.

--- js/initEchart.js
+++ js/initEchart.js
@@ -1,98 +1,156 @@
 // 初始化echarts
 function initEcharts(data) {
-	var myChart = echarts.init(document.getElementById("pulmonary"));
-	console.log(data);
-	var option = {
-		grid: {
-			left: '16%',
-			top: '5%'	,
-			bottom: '10%',
-			right: '5%'		
+    var myChart = echarts.init(document.getElementById("pulmonary"));
+    console.log(data);
+    // var option = {
+    //  grid: {
+    //      left: '16%',
+    //      top: '5%'   ,
+    //      bottom: '10%',
+    //      right: '5%'     
 
-		},	
-		tooltip: {
-				show: true,
-        trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'line' ,       // 默认为直线，可选为：'line' | 'shadow'
-            show: false
+    //  },  
+    //  tooltip: {
+    //      show: true,
+ //            trigger: 'axis',
+ //            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+ //                type : 'line' ,       // 默认为直线，可选为：'line' | 'shadow'
+ //                show: false
+ //            },
+ //            formatter: function(params){         
+ //             return params[0].name + ": " + params[0].data.toFixed(2) + "%"
+ //            }
+ //        },   
+ //     xAxis:[{
+ //            type:'category',//默认为类目
+ //            data : ["异常度","肺结核疑似度"],
+ //            axisLine:{
+ //                 show: false,
+ //                lineStyle:{
+ //                    color:'#3870a9',
+ //                    type: 'dotted'
+ //                }
+ //            },
+ //            axisLabel: {
+ //             fontSize: 16,
+ //             fontWeight: 'bold',
+ //             color: '#52a5f7',
+ //             fontFamily: 'Microsoft YaHei'
+ //            },
+ //            axisTick:{  
+ //        　　　　show: false  
+ //        　　}  
+ //        }],
+ //        yAxis : [
+ //            {
+ //                type : 'value',//默认为值类型
+ //                axisLine:{
+ //                    lineStyle:{
+ //                        color:'#3971a9'
+ //                    }
+ //                },
+ //                axisTick:{  
+ //            　　　　show:false  
+ //            　　},
+ //                 axisLabel: {
+ //                     formatter: '{value}%',
+ //                     fontSize: 16,
+ //                     color: '#52a5f7',
+ //                 fontFamily: 'Arial'
+ //                 },
+ //                 splitLine: {
+ //                     lineStyle: {
+ //                         type: 'dotted',
+ //                         color: '#3870a9'
+ //                     }
+ //                 },
+ //                 axisPointer: {
+ //                     label: "aaaa"
+ //                 },
+ //                 min: 0,
+ //                 max: 100 
+ //            }
+ //        ],
+ //        series : [
+ //            {
+ //                "name":"肺结核",
+ //                "type":"bar",
+ //                "itemStyle": {
+ //                 normal: {　　　　　　　　　　　　 //每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
+ //                    color: function(params) {
+ //                        /*var colorList = [colorData, colorData1];*/
+ //                        var colorList = colorData(data);
+ //                        return colorList[params.dataIndex];
+ //                    }
+ //                },
+ //                },
+ //                "data": data,
+ //                "barWidth": 56           
+ //            }
+ //        ]
+    // };
+
+    var option = {
+        grid: {
+         left: '16%',
+         top: '12%'   ,
+         bottom: '16%',
+         right: '16%'
+        }, 
+        tooltip: {
+            trigger: 'item',
+            formatter: function(data) {
+                let arr = data.seriesName.split(',');
+                return '肺结核' + '<br><span style="display: inline-block;margin-right: 5px;border-radius: 10px;width: 9px;height: 9px;background-color:' + data.color +'"></span>' + arr[data.dataIndex] + ': ' + data.value;
+            }
         },
-        formatter: function(params){        	
-        	return params[0].name + ": " + params[0].data.toFixed(2) + "%"
-        }
-    },	
-		xAxis:[{
-            type:'category',//默认为类目
-            data : ["异常度","肺结核疑似度"],
-            axisLine:{
-            		show: false,
-                lineStyle:{
-                    color:'#3870a9',
-                    type: 'dotted'
-                }
-            },
+        xAxis: {
+            type: 'value',
+            boundaryGap: [0, 1],
             axisLabel: {
-            	fontSize: 16,
-            	fontWeight: 'bold',
-            	color: '#52a5f7',
-            	fontFamily: 'Microsoft YaHei'
+             fontSize: 16,
+             color: '#3870a9',
+             fontFamily: 'Microsoft YaHei'
             },
-            axisTick:{  
-        　　　　show: false  
-        　　}  
-        }],
-    yAxis : [
-        {
-            type : 'value',//默认为值类型
-            axisLine:{
-                lineStyle:{
-                    color:'#3971a9'
+        },
+        yAxis: [
+            {
+                type: 'category',
+                data: ['陈旧', '正常', '正常'],
+                axisLabel: {
+                 fontSize: 16,
+                 color: '#fff',
+                 fontFamily: 'Microsoft YaHei'
+                },
+                
+            },
+            {
+                type: 'category',
+                data: ['活动', '结核', '异常'],
+                axisLabel: {
+                 fontSize: 16,
+                 color: '#fff',
+                 fontFamily: 'Microsoft YaHei'
+                },
+            }
+        ],
+        series:[{
+            name: '稳定性,疑似度,异常度',
+            type: 'bar',
+            data: [0.2, 0.6, 0.3],
+            itemStyle: {
+                normal: {
+                    barBorderRadius: [15, 15, 15, 15],
+                    color: '#4ad2ff'
                 }
             },
-            axisTick:{  
-        　　　　show:false  
-        　　},
-		        axisLabel: {
-		        	formatter: '{value}%',
-		        	fontSize: 16,
-		        	color: '#52a5f7',
-            	fontFamily: 'Arial'
-		        },
-		        splitLine: {
-		        	lineStyle: {
-		        		type: 'dotted',
-		        		color: '#3870a9'
-		        	}
-		        },
-		        axisPointer: {
-		        	label: "aaaa"
-		        },
-        		min: 0,
-        		max: 100 
-        }
-    ],
-    series : [
-        {
-            "name":"肺结核",
-            "type":"bar",
-            "itemStyle": {
-            	normal: {　　　　　　　　　　　　 //每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
-                color: function(params) {
-                    /*var colorList = [colorData, colorData1];*/
-                    var colorList = colorData(data);
-                    return colorList[params.dataIndex];
-                }
-            },
-            },
-            "data": data,
-            "barWidth": 56           
-        }
-    ]
-	};
+            barWidth: 20
+        }]
+    };
 
-	myChart.setOption(option);	
-	resize(myChart);
-}	
+    myChart.setOption(option);  
+    resize(myChart);
+}   
 
 // 用来调配颜色的
 function colorData(data) {
